26f411774113fb1fce1e39536a43c7d9f51d0149
Wal <wal@dcubed.(none)>
2010-07-28 09:48:37 +0100

    more robust signature handling & unique names per upload

--- lib/uploadify_s3_helper.rb
+++ lib/uploadify_s3_helper.rb
@@ -1,4 +1,6 @@
+
 module UploadifyS3Helper
+  include Signature  
   
   def uploadify_s3(options = {})
     stylesheet_link_tag('uploadify/uploadify') <<
@@ -22,39 +24,57 @@ module UploadifyS3Helper
   				'auto'           : true,
   				'multi'          : false,
   				'buttonText'		 : '#{options[:button_text]}',
+  				'buttonImg' 		 : '#{options[:button_img]}',
+  				'width' 	    	 : '#{options[:width]}',
+  				'height'      	 : '#{options[:height]}',  				
   				'sizeLimit'			 : '#{max_filesize}',
   				'fileDesc'		   : '#{options[:file_desc]}',				
   				'fileExt'				 : '#{options[:file_ext]}',
+  				'onSelect'       : function(event, queueID, fileObj) {
+  				    if (fileObj.size >= "#{max_filesize}") {
+      					$("#{options[:file_input_selector]}").uploadifyCancel(queueID);
+      					alert('Sorry the max file size is #{((max_filesize/1024)/1024)} MB');
+      					return false;
+      				}
+      				
+              $('div.button_group').hide();
+              return true;
+  				},
   				'onComplete'     : function(event, queueID, fileObj, response) {
+            $('div.button_group').show();
             fileInfo = {
 					    'name' : fileObj.name,
 					    'size' : fileObj.size,
 					    'type' : fileObj.type,
-					    'url'  : '#{bucket_url}#{upload_path}/' + fileObj.name
+					    'url'  : '#{bucket_url}#{upload_path}/' + fileObj.name + '#{rangen}'
 					  };  					  
 					  var onsucc = (#{options[:on_success]});
 					  onsucc(fileInfo);
 						$('#{options[:file_input_selector]}').hide();
-						return false;  				  
+						return true;
   				},
-  				'onError' 			 : function (event, queueID, fileObj, errorObj) {
+  				'onError' 			 : function (a, b, c, d) {
+            $('div.button_group').show();
   					if (d.info == 201) {
   					  fileInfo = {
-  					    'name' : fileObj.name,
-  					    'size' : fileObj.size,
-  					    'type' : fileObj.type,
-  					    'url'  : '#{bucket_url}#{upload_path}/' + fileObj.name
+  					    'name' : c.name,
+  					    'size' : c.size,
+  					    'type' : c.type,
+  					    'url'  : '#{bucket_url}#{upload_path}/' + c.name + '#{rangen}'
   					  };  					  
   					  var onsucc = (#{options[:on_success]});
   					  onsucc(fileInfo);
   						$('#{options[:file_input_selector]}').hide();
-  					} else {
+  					} else {         
               var onerror = (#{options[:on_error]});
               if (onerror) {
-                onerror(d.type, errorObj.text);                
+                onerror(d.type, d.text);    
+                $('#file_uploaderQueue').hide();
+                return false;           
               }              
   					}
-  					return true;  										
+  					
+  				 return true;
   				},				
           'scriptData' 		 : {
              'AWSAccessKeyId': '#{aws_access_key}',
@@ -62,7 +82,7 @@ module UploadifyS3Helper
              'acl': '#{acl}',
              'policy': '#{s3_policy}',
   					 'success_action_status': '201',
-             'signature': '#{s3_signature}',
+             'signature': encodeURIComponent(encodeURIComponent('#{s3_signature}')),
         		 'Content-Type': ''
             }        
   			});
@@ -75,15 +95,18 @@ module UploadifyS3Helper
   end
   
   def key
-    "#{upload_path}/${filename}"
+    "#{upload_path}/${filename}#{rangen}"
+  end
+  
+  def rangen
+    @rangen ||= rand(36 ** 8).to_s(36)
   end
   
   def policy_doc 
-    "{'expiration': '#{expiration_date}',
-        'conditions': [
-          {'bucket': '#{bucket}'},
-          ['starts-with', '$key', '#{upload_path}/'],
-          {'acl': '#{acl}'},
+    @policy ||= "{'expiration': '#{expiration_date}',
+        'conditions': [{'bucket': '#{bucket}'},
+          ['starts-with', '$key', '#{upload_path}'],
+          {'acl': '#{acl.to_s}'},
           ['content-length-range', 0, #{max_filesize}],
           {'success_action_status': '201'},
           ['starts-with','$folder',''],
@@ -94,18 +117,18 @@ module UploadifyS3Helper
   end
   
   def s3_policy
-    Base64.encode64(policy_doc).gsub(/\n|\r/, '')    
+    Base64.encode64(policy_doc).gsub(/\n|\r/, '')
   end
   
   def s3_signature
-    Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), aws_secret_key, s3_policy)).gsub(/\n| |\r/, '')
+    b64_hmac_sha1(aws_secret_key, s3_policy)
   end
 
   private
 
   def config
-     @config ||= load_config
-   end
+    @config ||= load_config
+  end
  
   def load_config
     YAML.load_file("#{RAILS_ROOT}/config/amazon_s3.yml")
@@ -132,16 +155,19 @@ module UploadifyS3Helper
   end
 
   def max_filesize
-    config['max_file_size'] || 50.megabyte
+    config['max_file_size'] || 20.megabyte
   end        
   
   def expiration_date
    10.hours.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')
-  end  
+ end  
   
   def default_options
     {
       :button_text => 'Add File',
+      :button_img => '/images/uploadify/upload.png',
+      :height => '50',
+      :width => '300',
       :file_ext => '*.*',
       :file_input_selector => '#file_upload',
       :file_desc => 'Please choose your file'
